4fb71e734ccedf314ef8051ab6eb2a175185034b
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2019-11-19 13:07:36 +0400

    improvement / add dropout parameter from the main

--- Homeworks/3_mnist/main.py
+++ Homeworks/3_mnist/main.py
@@ -11,7 +11,7 @@ tf.app.flags.DEFINE_string('val_images_dir', 'C:/_Files/MyProjects/ASDS_3/ASDS_D
 tf.app.flags.DEFINE_string('test_images_dir', 'C:/_Files/MyProjects/ASDS_3/ASDS_DL/Homeworks/3_mnist/_inputs_image/all/test/image/', 'Testing images data directory.')
 
 tf.app.flags.DEFINE_boolean('train', True, 'whether to train the network')
-tf.app.flags.DEFINE_integer('num_epochs', 30, 'epochs to train')
+tf.app.flags.DEFINE_integer('num_epochs', 80, 'epochs to train')
 tf.app.flags.DEFINE_integer('train_batch_size', 64, 'number of elements in a training batch')
 tf.app.flags.DEFINE_integer('val_batch_size', 64, 'number of elements in a validation batch')
 tf.app.flags.DEFINE_integer('test_batch_size', 64, 'number of elements in a testing batch')
@@ -33,13 +33,13 @@ tf.app.flags.DEFINE_integer('max_to_keep', 5, 'Number of checkpoint files to kee
 
 tf.app.flags.DEFINE_integer('summary_step', 100, 'Number of steps we cycle through before saving summary.')
 
-tf.app.flags.DEFINE_string('model_name', 'RNN', 'name of model')
+tf.app.flags.DEFINE_string('model_name', 'CNN', 'name of model')
 
 FLAGS = tf.app.flags.FLAGS
 
 
 def main(argv=None):
-    model = RNN(
+    model = DNN(
         train_images_dir=FLAGS.train_images_dir,
         val_images_dir=FLAGS.val_images_dir,
         test_images_dir=FLAGS.test_images_dir,
