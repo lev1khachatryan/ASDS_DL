66073a2ea501dd04369c9a2a6c0498e67d0d4ca3
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2019-11-26 21:13:56 +0400

    story / main of LSTM network testing - python main_2.py

--- Homeworks/3_mnist/main_2.py
+++ Homeworks/3_mnist/main_2.py
@@ -10,7 +10,7 @@ tf.app.flags.DEFINE_string('train_images_dir', 'C:/_Files/MyProjects/ASDS_3/ASDS
 tf.app.flags.DEFINE_string('val_images_dir', 'C:/_Files/MyProjects/ASDS_3/ASDS_DL/Homeworks/3_mnist/_inputs_image/all/validation/image/', 'Validation images data directory.')
 tf.app.flags.DEFINE_string('test_images_dir', 'C:/_Files/MyProjects/ASDS_3/ASDS_DL/Homeworks/3_mnist/_inputs_image/all/test/image/', 'Testing images data directory.')
 
-tf.app.flags.DEFINE_boolean('train', True, 'whether to train the network')
+tf.app.flags.DEFINE_boolean('train', False, 'whether to train the network')
 tf.app.flags.DEFINE_integer('num_epochs', 80, 'epochs to train')
 tf.app.flags.DEFINE_integer('train_batch_size', 64, 'number of elements in a training batch')
 tf.app.flags.DEFINE_integer('val_batch_size', 64, 'number of elements in a validation batch')
@@ -22,7 +22,7 @@ tf.app.flags.DEFINE_integer('num_channels', 1, 'Number of the channels of the im
 tf.app.flags.DEFINE_integer('num_classes', 10, 'Number of classes.')
 
 tf.app.flags.DEFINE_float('learning_rate', 0.001, 'Learning rate of the optimizer')
-tf.app.flags.DEFINE_float('keep_prob', 0.2, 'Keeping probability with dropout regularization')
+tf.app.flags.DEFINE_float('keep_prob', 0.33, 'Keeping probability with dropout regularization')
 
 tf.app.flags.DEFINE_integer('display_step', 100, 'Number of steps we cycle through before displaying detailed progress.')
 tf.app.flags.DEFINE_integer('validation_step', 100, 'Number of steps we cycle through before validating the model.')
@@ -33,7 +33,7 @@ tf.app.flags.DEFINE_integer('max_to_keep', 5, 'Number of checkpoint files to kee
 
 tf.app.flags.DEFINE_integer('summary_step', 100, 'Number of steps we cycle through before saving summary.')
 
-tf.app.flags.DEFINE_string('model_name', 'RNN', 'name of model')
+tf.app.flags.DEFINE_string('model_name', 'RNN_new', 'name of model')
 
 FLAGS = tf.app.flags.FLAGS
 
