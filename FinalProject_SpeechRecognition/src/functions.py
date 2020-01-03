df5aefdd79819eb77f82626810e84a109e7a2ee5
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2020-01-03 16:28:37 +0400

    story / Custom fast fourier transform, spectrogram and other useful functions

---
+++ FinalProject_SpeechRecognition/src/functions.py
@@ -0,0 +1,105 @@
+# Used functions
+
+# ==============================================================================
+"""
+This script contains a basic template for:
+    1. custom implementation of fast fourier transform
+    2. log spectrogramm
+    3. frequencies violin plot for data analysis
+    4. wave to image
+"""
+from scipy import signal
+from scipy.io import wavfile
+import scipy.misc
+from scipy.fftpack import fft
+import librosa
+
+import os
+from os.path import isdir, join
+import sys
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+
+from matplotlib.backend_bases import RendererBase
+import matplotlib.pyplot as plt
+import seaborn as sns
+import IPython.display as ipd
+import librosa.display
+import plotly.offline as py
+import plotly.graph_objs as go
+import plotly.tools as tls
+# %matplotlib inline
+from PIL import Image
+
+
+def custom_fft(y, fs):
+    T = 1.0 / fs
+    N = y.shape[0]
+    yf = fft(y)
+    xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+    vals = 2.0/N * np.abs(yf[0:N//2])  # FFT is simmetrical, so we take just the first half
+    # FFT is also complex, to we take just the real part (abs)
+    return xf, vals
+
+def log_specgram(audio, sample_rate, window_size=20,
+                 step_size=10, eps=1e-10):
+    nperseg = int(round(window_size * sample_rate / 1e3))
+    noverlap = int(round(step_size * sample_rate / 1e3))
+    freqs, times, spec = signal.spectrogram(audio,
+                                    fs=sample_rate,
+                                    window='hann',
+                                    nperseg=nperseg,
+                                    noverlap=noverlap,
+                                    detrend=False)
+    return freqs, times, np.log(spec.T.astype(np.float32) + eps)
+
+def violinplot_frequency(dirs, freq_ind):
+    spec_all = []  # Contain spectrograms
+    ind = 0
+    for direct in dirs:
+        spec_all.append([])
+
+        waves = [f for f in os.listdir(join(train_audio_path, direct)) if
+                 f.endswith('.wav')]
+        for wav in waves[:100]:
+            sample_rate, samples = wavfile.read(
+                train_audio_path + direct + '/' + wav)
+            freqs, times, spec = log_specgram(samples, sample_rate)
+            spec_all[ind].extend(spec[:, freq_ind])
+        ind += 1
+
+    # Different lengths = different num of frames. Make number equal
+    minimum = min([len(spec) for spec in spec_all])
+    spec_all = np.array([spec[:minimum] for spec in spec_all])
+
+    plt.figure(figsize=(13,7))
+    plt.title('Frequency ' + str(freqs[freq_ind]) + ' Hz')
+    plt.ylabel('Amount of frequency in a word')
+    plt.xlabel('Words')
+    sns.violinplot(data=pd.DataFrame(spec_all.T, columns=dirs))
+    plt.show()
+
+def wav2img(wav_path, targetdir='', figsize=(4,4)):
+    fig = plt.figure(figsize=figsize)    
+    # use soundfile library to read in the wave files
+    samplerate, test_sound  = wavfile.read(wav_path)
+    _, _, spectrogram = log_specgram(test_sound, samplerate)
+    
+    ## create output path
+    output_file = wav_path.split('/')[-1].split('.wav')[0]
+    output_file = targetdir +'/'+ output_file
+    #plt.imshow(spectrogram.T, aspect='auto', origin='lower')
+    plt.imsave('%s.png' % output_file, spectrogram)
+    plt.close()
+
+def wav2img_waveform(wav_path, targetdir='', figsize=(4,4)):
+    samplerate,test_sound  = wavfile.read(sample_audio[0])
+    fig = plt.figure(figsize=figsize)
+    plt.plot(test_sound)
+    plt.axis('off')
+    output_file = wav_path.split('/')[-1].split('.wav')[0]
+    output_file = targetdir +'/'+ output_file
+    plt.savefig('%s.png' % output_file)
+    plt.close()
